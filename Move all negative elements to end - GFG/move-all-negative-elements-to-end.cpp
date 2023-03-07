15ad3cfe60a79985c24442893434bd90de772333
Aditya Panat <34019010+adinovap20@users.noreply.github.com>
2023-03-07 21:52:09 +0530

    Added solution - LeetHub

--- Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.cpp
@@ -7,20 +7,17 @@ class Solution{
     public:
     void segregateElements(int arr[],int n)
     {
-        vector<int> tmp; vector<int> tmp2;
+        vector<int> neg; vector<int> pos;
         for(int i=0; i<n; i++){
-            if(arr[i]>=0){
-                tmp.push_back(arr[i]);
+            if(arr[i]<0){
+                neg.push_back(arr[i]);
             }else{
-                tmp2.push_back(arr[i]);
+                pos.push_back(arr[i]);
             }
         }
-        for(int i=0; i<tmp.size(); i++){
-            arr[i] = tmp[i];
-        }
-        for(int i=tmp.size(); i<n; i++){
-            arr[i] = tmp2[i-tmp.size()];
-        }
+        int lptr = 0;
+        for(auto x : pos) arr[lptr++] = x;
+        for(auto x : neg) arr[lptr++] = x;
     }
 };
 
